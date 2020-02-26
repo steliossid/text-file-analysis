61dc49bff7ba22146c68ef5ad6a232c1ccb85d84
Stylianos Sidiropoulos <stsid95@gmail.com>
2020-02-26 08:57:05 +0100

    small fixes

--- generate_text.py
+++ generate_text.py
@@ -1,6 +1,6 @@
 # Module that is responsible to generate text randomly based on the successor of a given word
 
-import time
+# import time
 import sys
 import re
 from random import choices
@@ -24,23 +24,29 @@ def generate_random_sentence(doc):
     words = doc.split()
     c = dict(Counter(words))
 
-    while word_counter < max_word_number:  # must add this (until there are no successors in cur_word)
-        rx = r'%s (\w+){1}' % cur_word
-        prog = re.compile(rx)
-        c2 = Counter(prog.findall(doc))
-        most_common_successors = dict(c2.most_common())
-        for key, value in most_common_successors.items():
-            most_common_successors[key] = value / c[cur_word]
-        selected_successor = choices(population=list(most_common_successors.keys()),
-                                     weights=list(most_common_successors.values()), k=1)
-        cur_word = selected_successor[0]
-        msg = msg + " " + cur_word
-        word_counter += 1
+    if cur_word in words:
+        while word_counter < max_word_number and cur_word != words[-1]:
+            # Until there are no successors in cur_word -> only the last word will not have a successor
+            rx = r'%s (\w+){1}' % cur_word
+            prog = re.compile(rx)
+            c2 = Counter(prog.findall(doc))
+            most_common_successors = dict(c2.most_common())
+            for key, value in most_common_successors.items():
+                most_common_successors[key] = value / c[cur_word]
+            selected_successor = choices(population=list(most_common_successors.keys()),
+                                         weights=list(most_common_successors.values()), k=1)
+            cur_word = selected_successor[0]
+            msg = msg + " " + cur_word
+            word_counter += 1
+
     return msg
 
 
 if __name__ == '__main__':
-    start_time = time.time()
+    # 2500 words takes 39.88 sec
+    # 500 words takes 11.58 sec
+
+    # start_time = time.time()
     main()
-    passed_time = time.time() - start_time
-    print(passed_time)
+    # passed_time = time.time() - start_time
+    # print(passed_time)
